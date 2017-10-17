bd67c24ad3138472efa703541c14384a3237b13f
Rizqi Taufiq <asus@192.168.56.1>
2017-10-17 10:19:43 +0700

    Tugas Rat In A Maze ganjil

---
+++ src/ratinamaze1/RatInAMaze1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ratinamaze1;
+
+/**
+ *
+ * @author asus
+ */
+public class RatInAMaze1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
